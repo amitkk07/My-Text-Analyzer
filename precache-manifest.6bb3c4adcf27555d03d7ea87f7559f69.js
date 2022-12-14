098e0c34fddf42973ddf33e4a10da32d46a77921
Amit <amitkanaujiya07@gmail.com>
2022-12-14 11:05:26 +0530

    Updates

---
+++ precache-manifest.6bb3c4adcf27555d03d7ea87f7559f69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8e33db7122d444b4147",
+    "url": "/My-Text-Analyzer/static/css/main.a98cbea6.chunk.css"
+  },
+  {
+    "revision": "e8e33db7122d444b4147",
+    "url": "/My-Text-Analyzer/static/js/main.e8e33db7.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/My-Text-Analyzer/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/My-Text-Analyzer/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8f3e7bca8fbb0081fb40",
+    "url": "/My-Text-Analyzer/static/js/runtime~main.8f3e7bca.js"
+  },
+  {
+    "revision": "bd504640534b323eaff21343707a49fb",
+    "url": "/My-Text-Analyzer/index.html"
+  }
+];
